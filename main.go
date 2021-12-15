e2b829139dcd9098a00105957a8318eb491a6b86
yanming.zhang <yanming.zhang@leyantech.com>
2021-12-15 14:21:56 +0800

    fix

--- main.go
+++ main.go
@@ -2,8 +2,8 @@ package main
 
 import (
 	"flag"
-	"github.com/AliyunContainerService/log-pilot/pilot"
-	log "github.com/Sirupsen/logrus"
+	"log-pilot/pilot"
+	log "github.com/sirupsen/logrus"
 	"io/ioutil"
 	"os"
 	"path/filepath"
